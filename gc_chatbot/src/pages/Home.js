f6fc548c5a2242d71897bf35870dd83a13bfb560
Aisha Najeeb <90481018+ayshanjb25@users.noreply.github.com>
2024-05-13 01:15:04 +0530

    first commit

---
+++ gc_chatbot/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <header className="App-header">
+        
+        <p>
+          ChatAppss
+        </p>
+        
+      </header>
+      
+    </div>
+  )
+}
+
+export default Home
